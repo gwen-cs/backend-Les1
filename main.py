c8785ff2060f4043b00b6a0facb9aa10ab822d6d
Gwenneke <gwenneke@proton.me>
2026-06-25 18:07:15 +0200

    werken met escape

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from xml.sax.saxutils import escape
+from markupsafe import escape
+
 from flask import Flask, jsonify, render_template,request
 
 app = Flask(__name__)
@@ -5,3 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
+
+@app.route('/hello/<name>')
+def groeten(name):
+    return f"Hello {escape(name)}"
+
+@app.route('/greetsfrom/<name>')
+def greetsfrom(name):
+    return render_template("greetsfrom.html", name=escape(name))
